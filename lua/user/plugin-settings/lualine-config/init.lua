2f2193950adcd6026dae2417186121a3ce2dd4a4
michael o'donovan <mikeodonovan3@gmail.com>
2022-02-24 21:33:33 +0000

    feat(lualine-plugin): reduce clutter on lualine

--- lua/user/plugin-settings/lualine-config/init.lua
+++ lua/user/plugin-settings/lualine-config/init.lua
@@ -10,7 +10,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch'},
     lualine_c = {'filename'},
     lualine_x = {''},
     lualine_y = {'progress'},
