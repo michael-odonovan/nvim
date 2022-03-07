877f074dc929f9c5aff7ebd2d3c9c8a27f971b9f
michael o'donovan <mikeodonovan3@gmail.com>
2022-03-07 10:50:59 +0000

    feat(goto-files-folders): reorganise shortcuts to files/folders

--- lua/user/goto-files-folders.lua
+++ lua/user/goto-files-folders.lua
@@ -1,40 +1,23 @@
 vim.cmd([[
 
-" Latest project
-:command! Project :e ~/RbcMobilityNextJs
+" Folders
+command! Project cd ~/RbcMobilityNextJs | :Ex
+command! Init cd ~/.config/nvim | :e init.lua
+command! Wiki cd ~/Wiki | :e index.md
+command! Cn cd ~/CodingNotes | :Ex
+command! Snippets cd ~/.config/nvim/snippets-luasnip-vscode | :Ex
+command! Blog cd ~/Blog | :Ex
+:command! Notes cd ~/Documents/notes-general | :Ex
+:command! Documents cd ~/Documents | :Ex
+:command! Scripts cd ~/bin | :Ex
+:command! Js cd ~/Javascript | :Ex
+:command! Desk cd ~/desktop | :Ex
+:command! Docs cd ~/documents | :Ex
+:command! Downloads cd ~/Downloads | :Ex
 
-" open init.lua
-:command! Init :e ~/.config/nvim/init.lua
-
-" open Snippets folder
-:command! Snippets :e ~/.config/nvim/snippets-luasnip-vscode
-
-function! Wiki()
-  cd ~/Wiki 
-  :e index.md
-endfunction
-command! Wiki :call Wiki()
-
-function! Cn()
-  cd ~/CodingNotes
-  :Ex
-endfunction
-command! Cn :call Cn()
-
-command! Wiki :call Wiki()
-:command! Coding :e ~/Wiki/Coding-Notes.md
-:command! Blog :e ~/Blog
-:command! Learning :e ~/Learning
-:command! Scratch :e ~/CodingNotes/scratch.md
+" Files
+:command! Scratch :e ~/scratch.md
 :command! Bashrc :e ~/.bashrc
 :command! Source :source ~/.config/nvim/init.lua
-:command! Cf :e ~/Coding
-:command! Notes :e ~/Documents/notes-general
-:command! Documents :e ~/Documents
-:command! Scripts :e ~/bin
-:command! Js :e ~/Javascript
-:command! Desk :e ~/desktop
-:command! Docs :e ~/documents
-:command! Downloads :e ~/Downloads
 
 ]])
