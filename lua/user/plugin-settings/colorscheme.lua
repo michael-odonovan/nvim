7a1bcb711f8166d224db51b07b07f522ca84bb77
michael o'donovan <mikeodonovan3@gmail.com>
2022-03-20 17:37:51 +0000

    feat(Snippets): add switch snippet

--- lua/user/plugin-settings/colorscheme.lua
+++ lua/user/plugin-settings/colorscheme.lua
@@ -10,17 +10,16 @@ nightfox.setup({
 		},
 	},
 	pallets = {
-		bg = "#111111",
-		bg_highlight = "#333333",
+		nightfox = {
+			bg1 = "#111111",
+			bg3 = "#333333",
+		},
 	},
 	groups = {
-		Folded = { fg = "c.blue", bg = "c.bg" }, -- color of highlight used for closed folds
+		Folded = { fg = "c.blue", bg = "c.bg1" }, -- color of highlight used for closed folds
 
 		-- Note: Markdown headings are now controlled by vimwiki settings!
 	},
 })
 
 vim.cmd("colorscheme nightfox")
-
--- local pallet = require("nightfox.pallet").load("nightfox")
--- print(vim.inspect(pallet.bg))
