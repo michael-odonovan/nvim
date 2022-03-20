3d4a568653ba488e716f995b2765011a068b163c
michael o'donovan <mikeodonovan3@gmail.com>
2022-03-20 10:20:08 +0000

    update

--- lua/user/plugin-settings/colorscheme.lua
+++ lua/user/plugin-settings/colorscheme.lua
@@ -1,23 +1,27 @@
--- Setup nvim-cmp.
 local status_ok, nightfox = pcall(require, "nightfox")
 if not status_ok then
-  return
+	return
 end
 
 nightfox.setup({
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-  },
-  colors = {
-    bg = "#111111",
-		bg_highlight = "#333333"
-  },
-  hlgroups = {
-    Folded = { fg = "c.blue", bg = "c.bg" }, -- color of highlight used for closed folds
+	options = {
+		styles = {
+			comments = "italic", -- change style of comments to be italic
+		},
+	},
+	pallets = {
+		bg = "#111111",
+		bg_highlight = "#333333",
+	},
+	groups = {
+		Folded = { fg = "c.blue", bg = "c.bg" }, -- color of highlight used for closed folds
 
-    -- Note: Markdown headings are now controlled by vimwiki settings!
-
-  }
+		-- Note: Markdown headings are now controlled by vimwiki settings!
+	},
 })
 
-nightfox.load()
+vim.cmd("colorscheme nightfox")
+
+local pallet = require("nightfox.pallet").load("nightfox")
+
+print(vim.inspect(pallet.red))
