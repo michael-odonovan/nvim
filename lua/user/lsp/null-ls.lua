e442251bfa100c10ea897a27eb4c5ec5b129851d
michael o'donovan <mikeodonovan3@gmail.com>
2022-05-20 21:53:59 +0100

    update

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,28 +1,27 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.prettier,
-		formatting.black,
-		formatting.stylua,
-		formatting.eslint,
-	},
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
+  debug = false,
+  sources = {
+    -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    -- formatting.prettier,
+    formatting.sumneko_lua,
+    formatting.eslint,
+  },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
         augroup LspFormatting
           autocmd! * <buffer>
           " autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 2000)
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
         augroup END
       ]])
-		end
-	end,
+    end
+  end,
 })
